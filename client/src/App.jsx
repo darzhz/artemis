d95f1c21d1c27d7b525eca2f104f28ceb4220594
darzhz <darzhz@protonmail.com>
2024-01-17 19:07:38 +0530

    update on ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,14 @@ import reactLogo from './assets/ARTEMIS.svg'
 import './App.css'
 import  Finput from './components/Finput'
 import RegistrationForm from './components/RegistrationForm'
+import Attendence from './components/Attendence'
 
 function App() {
   return (
     <>
       <img src={reactLogo} className="mx-auto mb-10 h-20 mt-0"/>
-      <RegistrationForm />
+      {/* <RegistrationForm /> */}
+      <Attendence/>
     </>
   )
 }
